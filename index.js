e6870b87efd38e99b421f51b190ba0e286393583
Raphae <raphae@pop-os.localdomain>
2022-02-25 09:48:43 -0300

    Database parameters set in dotenv and passed to index

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const { HOST, USERS, PASSWORD, DATABASE} = process.env
+
+console.log(HOST, USERS, PASSWORD, DATABASE)
 
 app.use(cors())
