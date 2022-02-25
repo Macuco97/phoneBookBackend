594c9cd164f1dd55e1b848f6b161335b9450b8f2
Raphae <raphae@pop-os.localdomain>
2022-02-25 10:29:13 -0300

    Fixing and setup dotenv and database

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const { HOST, USERS, PASSWORD, DATABASE} = process.env
+require('dotenv').config()
+const { HOST, USERS, PASSWORD, DATABASE }  = process.env
 
 console.log(HOST, USERS, PASSWORD, DATABASE)
 
 app.use(cors())
+
+commit e6870b87efd38e99b421f51b190ba0e286393583
+Author: Raphae <raphae@pop-os.localdomain>
+Date:   Fri Feb 25 09:48:43 2022 -0300
+
+    Database parameters set in dotenv and passed to index
